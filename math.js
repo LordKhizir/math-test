5ad3e0d8d8afa12ef76b811ef0efc9e5fdd74768
Toni Navarro <toni.navarro@infojobs.net>
2017-10-25 00:29:42 +0200

    Add counters for time and operations. Also, improve control of zeroes, animation...

--- math.js
+++ math.js
@@ -3,17 +3,20 @@ var boxContent = [0,1,2]; // 1 is first number, 2 is second number, 0 is result
 var upTo = [0,10,10];
 var guess = 0;
 var operation = 'x';
+var maxSeconds = 0;
+var maxOperations = 0;
+var GLOBAL_removeZeroes = 0;
 var counterOK = 0;
 var counterKO = 0;
 var resultTable;
 var finished = 0;
-
-const RESULT_DELAY = 1000;
+var secondsInterval = 0;
+var LAPSE_BETWEEN_OPERATIONS = 400;
 
 initialize();
 
 function initialize() {
-  addEvent(window, "keyup", commandKeyPress);
+  // Recover parameters
   upTo[1] = getParameterByName('firstNumber',10);
   upTo[2] = getParameterByName('secondNumber',10);
   var operationCode = getParameterByName('operation','MULT');
@@ -22,19 +25,48 @@ function initialize() {
   } else {
     operation = '+'
   }
+  maxSeconds = getParameterByName('maxSeconds',0);
+  maxOperations = getParameterByName('maxOperations',0);
+  GLOBAL_removeZeroes = getParameterByName('removeZeroes',0);
+
+  // Initialize counters
   counterOK = 0;
   counterKO = 0;
-  writeById('counter-ok',counterOK);
-  writeById('counter-ko',counterKO);
+  remainingSeconds = maxSeconds;
+  remainingOperations = maxOperations;
+
+  // UI initialization
   writeById('operation', operation);
+  writeById('counter-operations', remainingOperations);
+  writeById('counter-time', remainingSeconds);
   document.getElementById('play-area').className = '';
   generateResultTable(upTo[1],upTo[2]);
+
+  // set events, timeouts, intervals...
+  addEvent(window, "keyup", commandKeyPress);
+  if (maxSeconds>0) {
+    secondsInterval = setInterval(function() {oneSecondPassed()}, 1000);
+  }
+
+  // Go on
   newOperation();
 }
 
+function oneSecondPassed() {
+  remainingSeconds--;
+  writeById('counter-time', remainingSeconds);
+  if (remainingSeconds==0) {
+    clearInterval(secondsInterval);
+  }
+}
+
 function newOperation() {
-  number[1] = getRandomInt(0,upTo[1]);
-  number[2] = getRandomInt(0,upTo[2]);
+  var minValue = 0;
+  if (GLOBAL_removeZeroes) {
+    minValue = 1;
+  }
+  number[1] = getRandomInt(minValue,upTo[1]);
+  number[2] = getRandomInt(minValue,upTo[2]);
   var gotBackOnce = 0;
   // ONLY use this number if it has not been asked yet
   while (resultTable[number[1]][number[2]]!=0) {
@@ -80,24 +112,34 @@ function processDigit(digit) {
   // Same number of digits?
   if (guess.toString().length == number[elementToGuess].toString().length) {
       processResult(guess==number[elementToGuess] || (operation=='x' && number[0]==0 && elementToGuess!=0) );
-      setTimeout('newOperation()',RESULT_DELAY);
+      remainingOperations--;
+      writeById('counter-operations', remainingOperations);
+      setTimeout('newOperation();',LAPSE_BETWEEN_OPERATIONS); // Leave a little time, just for visual feedback
   }
+}
+
+function animateCounter(counterId) {
+  var elem = document.getElementById(counterId);
+  var originalClassname = elem.className.replace(' animation-counter-increase','');
+  elem.className = originalClassname;
+  setTimeout(function () {
+    elem.className = originalClassname + ' animation-counter-increase';
+  }, 100);
 }
 
 function processResult(result) {
   var resultCellKey = 'result-' + number[1] + '-' + number[2];
   if (result) {
-    document.getElementById(resultCellKey).className = 'cell-ok';
+    document.getElementById(resultCellKey).className = 'cell-ok'; // TODO remove
     resultTable[number[1]][number[2]] = 1;
-    document.getElementById('result').className = 'result ok';
     counterOK = counterOK + 1;
+    animateCounter('img-counter-ok');
     writeById('counter-ok',counterOK);
   } else {
-    document.getElementById(resultCellKey).className = 'cell-ko';
+    document.getElementById(resultCellKey).className = 'cell-ko'; // TODO remove
     resultTable[number[1]][number[2]] = -1;
-    document.getElementById('result').className = 'result ko';
-
     counterKO = counterKO + 1;
+    animateCounter('img-counter-ko');
     writeById('counter-ko',counterKO);
   }
 }
@@ -127,16 +169,21 @@ function showNumberContent(index, content) {
 /* Generate result table, PLUS its representation */
 function generateResultTable(firstNumber, secondNumber) {
   resultTable = new Array(firstNumber);
-  var table = document.getElementById('result-table');
+  var table = document.getElementById('result-table'); // TODO remove
   for (var y=0;y<=firstNumber;y++) {
-    var row = table.insertRow(y);
+    var row = table.insertRow(y);  // TODO remove
     resultTable[y] = new Array(secondNumber);
     for (var x=0;x<=secondNumber;x++) {
-      resultTable[y][x] = 0; // 0 means "not set yet"
-      var cell = row.insertCell(x);
-      cell.id = 'result-' + y + '-' + x;
-      cell.innerHTML = y + 'x' + x;
-      cell.className = 'cell-pending';
+      var cell = row.insertCell(x);  // TODO remove
+      cell.id = 'result-' + y + '-' + x;  // TODO remove
+      cell.innerHTML = y + 'x' + x;  // TODO remove
+      if (GLOBAL_removeZeroes==1 && (x==0 || y==0)) {
+        resultTable[y][x] = -2; // -2 means "NOT ASKED"
+        cell.className = 'cell-notasked'; // TODO remove
+      } else {
+        resultTable[y][x] = 0; // 0 means "not set yet"
+        cell.className = 'cell-pending';  // TODO remove
+      }
     }
   }
 }
